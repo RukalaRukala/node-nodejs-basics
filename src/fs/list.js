d2deb86a88d3ec401457af10985abac32e954531
Mikalai Rusakovich <kolyos83@mail.ru>
2024-01-27 15:27:35 +0300

    feat: implement list.js - task 5

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+import fs from "fs/promises";
+
 const list = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const folder = [__dirname, '/files'].join('');
+    try {
+        await fs.access(folder);
+        const filesName = await fs.readdir(folder)
+
+        console.log('Имена файлов в папке:\n' + filesName.join('\n'))
+    } catch (err) {
+        console.error('FS operation failed:\n' + err);
+    }
 };
 
 await list();
