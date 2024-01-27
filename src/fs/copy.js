9fcd93a182cc6c95f43cc0ed28bd14e5f310772f
Mikalai Rusakovich <kolyos83@mail.ru>
2024-01-27 12:25:39 +0300

    feat: implement copy.js - task 2

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,41 @@
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+import fs from "fs/promises";
+import * as path from "path";
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        const __dirname = dirname(fileURLToPath(import.meta.url));
+        const __folderCopy= path.join(__dirname, '/files_copy');
+        const __folderInit = path.join(__dirname, '/files')
+
+        const isFolderCopyExists = await fs.access(__folderCopy)
+            .then(() => true)
+            .catch(() => false);
+
+        if (isFolderCopyExists) {
+            throw new Error('FS operation failed: Folder already exists');
+        }
+
+        await fs.mkdir(__folderCopy)
+        const files = await fs.readdir(__folderInit);
+
+        const copyPromises = files.map(async (file) => {
+            const filePath = path.join(__folderInit, file);
+            const stats = await fs.stat(filePath);
+
+            if (stats.isFile()) {
+                await fs.copyFile(filePath, path.join(__folderCopy, file));
+            }
+        });
+
+        await Promise.all(copyPromises);
+
+        console.log('Folder created successfully');
+    }
+    catch (error) {
+        console.error(error.message);
+    }
 };
 
 await copy();
