383f4dd4b5f0db9a3ec29d214c609f38fcb9d57e
Mikalai Rusakovich <kolyos83@mail.ru>
2024-01-27 14:56:48 +0300

    feat: implement delete.js - task 4

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+import fs from "fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const deletedFile = [__dirname, '/files/fileToRemove.txt'].join('');
+    const isDeletedFileExists = await fs.access(deletedFile).then(() => true).catch(() => false);
+    try {
+        isDeletedFileExists
+            ? await fs.unlink(deletedFile)
+                .then(() => console.log('File remove successfully'))
+            : await Promise.reject(new Error('FS operation failed:\nfile "fileToRemove.txt" does not exist'))
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await remove();
