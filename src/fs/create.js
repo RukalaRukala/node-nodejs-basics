ab196e0428a2cc7be940799695f775322751dd8c
Mikalai Rusakovich <kolyos83@mail.ru>
2024-01-27 11:22:10 +0300

    feat: implement create.js - task 1

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from 'fs/promises';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const __dirname = dirname(fileURLToPath(import.meta.url));
+        const __filename = [__dirname, '/files/fresh.txt'].join('');
+
+        const isFileExists = await fs.access(__filename).then(() => true).catch(() => false);
+
+        if (isFileExists) {
+            throw new Error('FS operation failed: File already exists');
+        }
+        
+        await fs.writeFile(__filename, 'I am fresh and young');
+
+        console.log('File created successfully');
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 await create();
