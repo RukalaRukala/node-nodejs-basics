84e3f891f8fe7632101968b5e51d5b654793fb95
Mikalai Rusakovich <kolyos83@mail.ru>
2024-01-28 23:22:58 +0300

    feat: implement read.js - task 11

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,26 @@
+import path from "path";
+import {fileURLToPath} from "url";
+import fs from "fs";
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const file= path.join(__dirname, '/files/fileToRead.txt');
+    const stream = fs.createReadStream(file);
+
+    await new Promise((resolve, reject) => {
+        stream.on('data', (chunk) => {
+            process.stdout.write(chunk);
+        });
+
+        stream.on('end', () => {
+            stream.destroy();
+        });
+
+        stream.on('error', (error) => {
+            reject(error);
+        });
+    });
+
 };
 
 await read();
