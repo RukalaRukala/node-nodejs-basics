afbb506206e94cfc52f731b0d2f5b232fc3f253a
Mikalai Rusakovich <kolyos83@mail.ru>
2024-01-29 23:58:15 +0300

    feat: implement worker.js and main.js - tasks 16,17

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import * as os from "os";
+import { Worker } from 'worker_threads';
+import path from "path";
+import {fileURLToPath} from "url";
+
 const performCalculations = async () => {
-    // Write your code here
+    try {
+        const __dirname = path.dirname(fileURLToPath(import.meta.url));
+        const workerPath= path.join(__dirname, '/worker.js');
+        const numCores = os.cpus().length;
+        const results = new Array(numCores);
+
+        const createWorker = (workerIndex, n) => {
+            return new Promise((resolve, reject) => {
+                const worker = new Worker(workerPath, { workerData: n });
+
+                worker.on('message', (result) => resolve(result));
+
+                worker.on('error', (error) => {
+                    console.error(`Worker ${workerIndex} error:`, error);
+                    reject(error);
+                });
+            });
+        };
+
+        for (let i = 0; i < numCores; i++) {
+            results[i] = await createWorker(i, 10 + i)
+        }
+        console.log('Results:', results);
+    } catch (error) {
+        console.error('Error:', error);
+    }
 };
 
 await performCalculations();
