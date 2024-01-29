02aacd3780515adc1e32e338e307c0d7c7fdc8e4
Mikalai Rusakovich <kolyos83@mail.ru>
2024-01-30 01:09:15 +0300

    feat: implement cp.js and script.js - tasks 18

--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -3,6 +3,9 @@ const args = process.argv.slice(2);
 console.log(`Total number of arguments is ${args.length}`);
 console.log(`Arguments: ${JSON.stringify(args)}`);
 
+console.log('\nPlease, input some phrase to check passing data from main process to child process and click "Enter"');
+console.log('Or input "CLOSE" to exit');
+
 const echoInput = (chunk) => {
     const chunkStringified = chunk.toString();
     if (chunkStringified.includes('CLOSE')) process.exit(0);
