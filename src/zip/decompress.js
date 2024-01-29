ec890170e1769579bc99a4e29ece3666784dcd09
Mikalai Rusakovich <kolyos83@mail.ru>
2024-01-29 03:36:20 +0300

    feat: implement decompress.js - task 15

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,36 @@
+import path from "path";
+import {fileURLToPath} from "url";
+import fs from "fs";
+import zlib from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const zipFile= path.join(__dirname, '/files/archive.gz');
+    const outputFile= path.join(__dirname, '/files/fileToCompress.txt');
+
+    fs.readFile(zipFile, (err, data) => {
+        if (err) {
+            console.error('Error reading the file:', err);
+            return;
+        }
+
+        const zipBuffer = Buffer.from(data);
+
+        zlib.gunzip(zipBuffer, (err, outputBuffer) => {
+            if (err) {
+                console.error('Error compressing the data:', err);
+                return;
+            }
+
+            fs.writeFile(outputFile, outputBuffer, (err) => {
+                if (err) {
+                    console.error('Error writing the compressed file:', err);
+                    return;
+                }
+                console.log('File decompressed successful:', outputFile);
+            });
+        });
+    });
 };
 
 await decompress();
